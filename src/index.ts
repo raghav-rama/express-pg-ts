a8368f871d1f456d1a9c3257a6a4a1b41e7cc85e
raghav-rama <hackerer528@gmail.com>
2023-04-23 05:24:05 +0530

    exec: `npx prettier --write .`

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 
 const PORT = process.env.PORT ?? 8000;
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-	res.status(200).json({message: "Hello World!"});
+  res.status(200).json({ message: 'Hello World!' });
 });
 
 app.listen(PORT, () => {
-	console.log(`app running on PORT ${PORT}`);
+  console.log(`app running on PORT ${PORT}`);
 });
-
